2088db7045104df8b8b49552d96d504cdf2fdbae
abderrahimself <abdemabrouk01@gmail.com>
2025-09-24 20:13:08 +0100

    Aggregated Endpoint Fixed

--- accounts/src/main/java/net/abderrahimself/accounts/service/client/CardsFallBack.java
+++ accounts/src/main/java/net/abderrahimself/accounts/service/client/CardsFallBack.java
@@ -10,6 +10,7 @@ public class CardsFallBack implements CardsFeignClient{
 
     @Override
     public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
-        return null;
+        // Return empty/default response instead of null
+        return ResponseEntity.ok(null); // Or return a default CardsDto
     }
 }
